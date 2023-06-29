e63f18d796051143d0be66085cf1ffc6546524fc
Patricio Milla <patojano36@gmail.com>
2023-06-28 23:44:17 -0300

    Estructuración del Proyecto

---
+++ resources/views/sitio/index.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+
+@include('sitio.estructura.head')
+
+<body>
+
+  @include('sitio.estructura.menu')
+
+  <main id="main">
+
+    @include('sitio.estructura.secciones.header')
+    @include('sitio.estructura.secciones.about')
+    @include('sitio.estructura.secciones.servicios')
+    @include('sitio.estructura.secciones.portafolio')
+    @include('sitio.estructura.secciones.precio')
+    @include('sitio.estructura.secciones.contact')
+
+  </main><!-- End #main -->
+
+  @include('sitio.estructura.footer')
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  @include('sitio.estructura.scripts')
+
+</body>
+
+</html>
